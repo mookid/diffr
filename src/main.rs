7a1c395b42a2520e89b16e3fa271a6c903c60ca1
Nathan Moreau <nathan.moreau@m4x.org>
2020-07-13 21:46:55 +0200

    diffr: fix bad highlighting, take 2. (#61)
    see 5fd84d1cc0f694ab5ea61ce75e6c871a33617e41 for an example of change.

--- src/main.rs
+++ src/main.rs
@@ -456,14 +456,16 @@ impl<'a> HunkBuffer<'a> {
     }
 
     fn push_aux(&mut self, line: &[u8], added: bool) {
+        // XXX: skip leading token
         let mut ofs = self.lines.len() + 1;
         add_raw_line(&mut self.lines, line);
         // get back the line sanitized from escape codes:
         let line = &self.lines.data()[ofs..];
-        // XXX: skip leading token and leading spaces
-        while ofs < line.len() && line[ofs].is_ascii_whitespace() {
-            ofs += 1
-        }
+        // skip leading spaces
+        ofs += line
+            .iter()
+            .take_while(|ch| ch.is_ascii_whitespace())
+            .count();
         diffr_lib::tokenize(
             &self.lines.data(),
             ofs,
